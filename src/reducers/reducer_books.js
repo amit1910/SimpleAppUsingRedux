bba5f0500e39cb6bf5ee5c68e8898585608046cd
Avinash Bhosale <34508451+avinashbhosale998@users.noreply.github.com>
2018-05-07 17:07:34 +0530

    initial commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function(){
+    return [
+        {title : 'javascript: the good parts', pages: 101 },
+        {title : 'harry potter', pages: 39 },
+        {title : 'the dark tower', pages: 85 },
+        {title : 'Eloquent Ruby', pages: 1 }
+    ]
+}
